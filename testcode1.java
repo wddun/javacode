5b7a89f027f34caf5cfbc24cf87d0b86d322f544
William Dunning <willdunning01@gmail.com>
2023-01-31 07:52:42 -0500

    initial file

---
+++ testcode1.java
@@ -0,0 +1,17 @@
+public class testcode1
+{
+   public static void main(String[] args)
+   {
+       double temperature = 70.5;
+       int tvChannel = 101;
+       boolean sunny = true;
+
+       System.out.print("Welcome to the weather report on Channel ");
+       System.out.println(tvChannel);
+       System.out.print("The temperature today is ");
+       System.out.println(temperature);
+       System.out.print("Is it sunny today? ");
+       System.out.println(sunny);
+   }
+}
+
