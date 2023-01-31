f4186c198fa11852f7e4d71bab833ad120fc0d75
William Dunning <willdunning01@gmail.com>
2023-01-31 14:11:30 -0500

    testcounter

--- testcode1.java
+++ testcode1.java
@@ -2,18 +2,14 @@ public class testcode1
 {
    public static void main(String[] args)
    {
-int x = 0;
-int y = 5;
-int z = 1;
-x++;
-y -= 3;
-z = x + z;
-x = y * z;
-y %= 2;
-z--;
-System.out.print (x);
-System.out.print (y);
-System.out.print (z);
+
+      int i = 0;
+      while (i < 10000) {
+        System.out.println(i);
+        i++;
+      }
+
+
    }
 }
 
