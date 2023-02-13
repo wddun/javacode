e6ef302ca852409037fa19668468f0406d8aa066
William Dunning <happyfirefly1@gmail.com>
2023-02-13 08:19:15 -0500

    vars

--- selfstorage.java
+++ selfstorage.java
@@ -6,9 +6,11 @@ public class selfstorage
         Scanner scan = new Scanner (System.in);
         System.out.print("Enter the number of paint cans:");
         int numCans = scan.nextInt();
-        int numDoors = numCans * 5;
+        final int SQR_FT_PER_CAN = 400;
+        final int SQR_FT_PER_DOOR = 80;
+        int totalSqrFeet = SQR_FT_PER_CAN/SQR_FT_PER_DOOR * numCans;
         System.out.println("Cans purchased by the manager:" + numCans);
-        System.out.print(numDoors + " doors can be completely painted.");
+        System.out.print(totalSqrFeet + " doors can be completely painted.");
     }
 
 }
