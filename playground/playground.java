8acb24d961e3868950ff79000830a1086e943c91
William Dunning <willdunning01@gmail.com>
2023-03-06 07:47:15 -0500

    loopcode

---
+++ playground/playground.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class playground 
+{
+
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        final int SENTINEL = 0;
+        System.out.print("Enter integer (0 to stop):");
+        int entry = scan.nextInt();
+        while (entry != SENTINEL)
+        {
+            System.out.print("Enter another (0 to stop):");
+            entry = scan.nextInt();
+        }
+        // assertion: To get to here, last entry MUST be 0
+
+    }
+    
+}
